881c5c0fbdab45376f5db6106fadcf63523ec412
JonDai <jondai@aliyun.com>
2016-07-08 15:10:25 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,42 @@
+<template>
+<nav class="navbar navbar-default">
+  <div class="container-fluid">
+    <div class="navbar-header">
+      <a class="navbar-brand" href="#">HuntDog</a>
+    </div>
+
+    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+      <ul class="nav navbar-nav">
+        <li class="active"><a href="#">首页 <span class="sr-only">(current)</span></a></li>
+        <li><a href="#">关于</a></li>
+     
+      </ul>
+
+    </div><!-- /.navbar-collapse -->
+  </div><!-- /.container-fluid -->
+</nav>
+ <!--  <div id="app">
+    <h1>{{ msg }}</h1>
+    <h2>Java 可能是世界上最好的语言之一</h2>
+  </div> -->
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      // note: changing this line won't causes changes
+      // with hot-reload because the reloaded component
+      // preserves its current state and we are modifying
+      // its initial state.
+      msg: 'Hello Vue!'
+    }
+  }
+}
+</script>
+
+<style>
+body {
+  font-family: Helvetica, sans-serif;
+}
+</style>
